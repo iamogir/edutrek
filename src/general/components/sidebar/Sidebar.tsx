4ab821bff10b198ccf93187e9e5709b27910ed57
iamogir <ia.mogirevsky@gmail.com>
2024-10-02 01:57:48 +0300

    created templates for sidebar

--- src/general/components/sidebar/Sidebar.tsx
+++ src/general/components/sidebar/Sidebar.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Active from "../../../modules/active/presentation/pages/Active.tsx";
+import Contacts from "../../../modules/contacts/presentation/pages/Contacts.tsx";
+import Students from "../../../modules/students/presentation/pages/Students.tsx";
+import Lecturers from "../../../modules/lecturers/presentation/pages/Lecturers.tsx";
+import Groups from "../../../modules/groups/presentation/pages/Groups.tsx";
 
 const Sidebar = () => {
     return (
         <div>
-            sidebar
+            <Active/>
+            <Contacts/>
+            <Groups/>
+            <Students/>
+            <Lecturers/>
         </div>
     );
 };
