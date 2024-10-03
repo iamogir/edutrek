06ae8a2865892c803dcf7ce212489138f52fb458
iamogir <ia.mogirevsky@gmail.com>
2024-10-03 15:49:36 +0300

    moved logo

--- src/general/components/header/Header.tsx
+++ src/general/components/header/Header.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import Search from "./search/Search.tsx";
 import style from "./header.module.css"
-import logo from "../../../icons/header/logo.webp"
+import logo from "../../../images/header/logo.webp"
 import LogoutIcon from "../../../icons/header/logout.svg?react"
 import {Link} from "react-router-dom";
 
