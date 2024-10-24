d357e6ae188140bbf90b9545dc39158e5c6d7e0a
iamogir <ia.mogirevsky@gmail.com>
2024-10-25 00:58:30 +0300

    fixed types and created human type

---
+++ src/general/components/types.ts
@@ -0,0 +1,10 @@
+export type HumanCardInfo = {
+    id: number
+    name: string,
+    phoneNum: string,
+    whatsApp?: string,
+    email: string,
+    location: string,
+    courseAbbreviation: string,
+    notify: boolean
+}
