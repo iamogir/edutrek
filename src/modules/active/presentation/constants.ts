a8c18753a0ef32a6cb71dd234ae54e7c8d5c60e3
iamogir <ia.mogirevsky@gmail.com>
2024-10-25 00:59:18 +0300

    styles fixed

--- src/modules/active/presentation/constants.ts
+++ src/modules/active/presentation/constants.ts
@@ -1,4 +1,5 @@
-import DropList from "./redux/types.ts";
+import DropList from "../domain/model/dropList.ts";
+import {HumanCardInfo} from "../../../general/components/types.ts";
 
 export const statusArr: DropList[] = [
     {
@@ -63,5 +64,56 @@ export const groupArr: DropList[] = [
     {
         id: 9,
         name: "QA-34 Rehovot"
+    }
+]
+
+export const humansArray: HumanCardInfo[] = [
+    {
+        id: 1,
+        name: "My Name",
+        phoneNum: "+0987654321",
+        whatsApp: "+0987654321",
+        email: "email@email.com",
+        location: "Tower",
+        courseAbbreviation: "FSD",
+        notify: false
+    },
+    {
+        id: 2,
+        name: "His Name",
+        phoneNum: "+1234567890",
+        email: "no@mail.il",
+        location: "City",
+        courseAbbreviation: "FSD",
+        notify: false
+    },
+    {
+        id: 3,
+        name: "Her Name",
+        phoneNum: "+0192837465",
+        whatsApp: "+0192837465",
+        email: "post@el.com",
+        location: "Earth",
+        courseAbbreviation: "QA",
+        notify: true
+    },
+    {
+        id: 4,
+        name: "Their Name",
+        phoneNum: "+5647382910",
+        whatsApp: "+5647382910",
+        email: "mail@email.il",
+        location: "USA",
+        courseAbbreviation: "QA",
+        notify: true
+    },
+    {
+        id: 5,
+        name: "God Name",
+        phoneNum: "+0912873465",
+        email: "ooo@ppp.pup",
+        location: "Heaven",
+        courseAbbreviation: "BP",
+        notify: false
     }
 ]
--- src/modules/active/presentation/pages/Active.tsx
+++ src/modules/active/presentation/pages/Active.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import style from "./active.module.css";
-import EntityHuman from "../components/entityHuman/EntityHuman.tsx";
+import HumanCard from "../../../../general/components/humanCard/HumanCard.tsx";
+import {humansArray} from "../constants.ts";
 
 const Active = () => {
 
-    let array: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
     return (
        <div className={style.box}>
-           {array.map((el) => <EntityHuman key={el} serial={el}/>)}
+           {humansArray.map((el) => <HumanCard key={el.id} info={el}/>)}
        </div>
     );
 };
